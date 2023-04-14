c62952f405e63110eed1a6dd2951ab6e523069de
Rodrigo Limões <rolimoes@gmail.com>
2023-04-14 08:01:28 -0300

    feat: Adding routes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import React, { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const AppLayout = lazy(() => import('@layouts/AppLayout'));
+
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  )
+	return (
+		<Routes>
+			<Route path={'/*'} element={<AppLayout />} />
+		</Routes>
+	);
 }
 
-export default App
+export default App;
