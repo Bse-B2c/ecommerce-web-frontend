d4cf0145edf241b08c34b9c1e62bbc2ae0bc2efb
Rodrigo Limões <rolimoes@gmail.com>
2023-04-14 00:06:17 -0300

    feat: Adding AppLayout

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,19 @@
+import React, {FC} from "react";
+import {Box, CssBaseline} from "@mui/material";
+import AppHeader from "@layouts/AppHeader";
+import AppBody from "@layouts/AppBody";
+
+interface AppLayoutStateProps{}
+interface AppLayoutDispatchProps{}
+
+type AppLayoutProps = AppLayoutStateProps & AppLayoutDispatchProps;
+
+const AppLayout: FC<AppLayoutProps> = () =>{
+    return <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppHeader/>
+        <AppBody/>
+    </Box>
+}
+
+export default AppLayout;
