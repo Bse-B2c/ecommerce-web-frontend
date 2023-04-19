28070cc0fb7533edc9f29e7210ccd56550822d19
Rodrigo Limões <rolimoes@gmail.com>
2023-04-19 10:56:04 -0300

    feat: Adding categoryApi; Adding getAllCategories query

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { productApi } from '@store/api/productApi';
 import { ratingApi } from '@store/api/ratingApi';
+import { categoryApi } from '@store/api/categoryApi';
 
 export const store = configureStore({
 	reducer: {
 		[productApi.reducerPath]: productApi.reducer,
 		[ratingApi.reducerPath]: ratingApi.reducer,
+		[categoryApi.reducerPath]: categoryApi.reducer,
 	},
 	devTools: true,
 	middleware: getDefaultMiddleware =>
 		getDefaultMiddleware().concat([
 			productApi.middleware,
 			ratingApi.middleware,
+			categoryApi.middleware,
 		]),
 });
 
