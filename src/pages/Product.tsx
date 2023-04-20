c60977b0dd33ffbf13ee7ce58fe7d66aa8371236
Rodrigo Limões <rolimoes@gmail.com>
2023-04-20 10:10:44 -0300

    feat: Adding back button

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from 'react';
 import { ProductItem } from '@features/Product';
+import { Button, Grid } from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 interface ProductStateProps {}
 interface ProductDispatchProps {}
@@ -7,7 +10,24 @@ interface ProductDispatchProps {}
 type ProductProps = ProductStateProps & ProductDispatchProps;
 
 const Product: FC<ProductProps> = () => {
-	return <ProductItem />;
+	const navigate = useNavigate();
+
+	return (
+		<Grid container direction={'column'} spacing={2} item xs>
+			<Grid item xs>
+				<Button
+					color={'secondary'}
+					size={'small'}
+					startIcon={<ArrowBack />}
+					onClick={() => navigate(-1)}>
+					Back
+				</Button>
+			</Grid>
+			<Grid item xs>
+				<ProductItem />
+			</Grid>
+		</Grid>
+	);
 };
 
 export default Product;
