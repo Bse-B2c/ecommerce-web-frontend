bb8a2f8d1cd5074e963434f4f9ed50140e3f52aa
Rodrigo Limões <rolimoes@gmail.com>
2023-04-20 10:12:34 -0300

    feat: Adding Products Page

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,14 @@
 import { lazy } from 'react';
 
 const Product = lazy(() => import('@pages/Product'));
+const Products = lazy(() => import('@pages/Products'));
 
 export const routes = [
+	{
+		name: '/Products',
+		path: '/product',
+		element: Products,
+	},
 	{
 		name: 'Product',
 		path: '/product/:id',
