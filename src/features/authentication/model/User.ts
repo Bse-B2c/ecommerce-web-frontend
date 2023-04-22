c72329fb3cd6355f1ac5c24b3ac3f253e0417307
Rodrigo Limões <rolimoes@gmail.com>
2023-04-22 12:20:56 -0300

    feat: Adding User type

---
+++ src/features/authentication/model/User.ts
@@ -0,0 +1,24 @@
+export interface Addresses {
+	id: number;
+	zipCode: string;
+	streetName: string;
+	houseNumber: number;
+	apartment: string;
+	city: string;
+	region: string;
+	country: string;
+	active: boolean;
+}
+
+export interface User {
+	id: number;
+	name: string;
+	email: string;
+	password?: string;
+	phone: string;
+	cpf: string;
+	brithDate: string;
+	createdAt: string;
+	addresses: Array<Addresses>;
+	roles: Array<number>;
+}
