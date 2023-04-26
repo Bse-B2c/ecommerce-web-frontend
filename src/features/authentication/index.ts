9fe5d7b4d618b6204023b40cf924a350fe9c3cfb
Rodrigo Limões <rolimoes@gmail.com>
2023-04-26 10:14:19 -0300

    feat: Exporting Address type

--- src/features/authentication/index.ts
+++ src/features/authentication/index.ts
@@ -5,7 +5,7 @@ import {
 	removeTokens,
 } from '@features/authentication/utils/utilsAuth';
 import { Tokens } from '@features/authentication/model/Tokens';
-import { User } from '@features/authentication/model/User';
+import { User, Addresses } from '@features/authentication/model/User';
 
-export type { Tokens, User };
+export type { Tokens, User, Addresses };
 export { getToken, getRefreshToken, removeTokens, setTokens };
