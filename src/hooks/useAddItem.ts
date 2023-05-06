41b11e7800f837a9cbe67189168a66f2d8289b88
Rodrigo Limões <rolimoes@gmail.com>
2023-05-06 18:45:47 -0300

    feat: Adding useAddItem hooks

---
+++ src/hooks/useAddItem.ts
@@ -0,0 +1,72 @@
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useAddItemMutation } from '@store/api/orderApi';
+import { ApiResponse } from '@src/model/ApiResponse';
+import { showNotification } from '@store/notification/notificationSlice';
+import { useAuthUser } from '@hooks/useAuthUser';
+
+interface Item {
+	productId: number;
+	price: number;
+	discount?: number;
+}
+
+export const useAddItem = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const [addItem] = useAddItemMutation();
+	const { user } = useAuthUser();
+
+	const addProductInCart = async (
+		isBuyNowMode: boolean,
+		{ productId, discount, price }: Item
+	) => {
+		try {
+			if (user && user.id) {
+				const response = await addItem({
+					productId: productId,
+					price,
+					discount: discount || undefined,
+				});
+
+				const { error } = response as unknown as { error: ApiResponse<null> };
+
+				if (error) {
+					dispatch(
+						showNotification({
+							type: 'error',
+							message: error.error?.message || 'Something went wrong',
+						})
+					);
+				} else {
+					if (isBuyNowMode) navigate('/shopcart');
+
+					dispatch(
+						showNotification({
+							type: 'success',
+							message: 'Product added successfully',
+						})
+					);
+				}
+			} else {
+				dispatch(
+					showNotification({
+						type: 'warning',
+						message: 'Please sign In or sign Up first',
+					})
+				);
+
+				navigate('/login');
+			}
+		} catch (e) {
+			const error = e as { data: ApiResponse<null> };
+			const message = error?.data?.error
+				? error.data.error.message
+				: 'Something went wrong';
+
+			dispatch(showNotification({ type: 'error', message }));
+		}
+	};
+
+	return { addProductInCart };
+};
